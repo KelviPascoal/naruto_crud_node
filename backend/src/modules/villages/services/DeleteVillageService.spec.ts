e76f751b0f4cbca912e4a2d1e410f9f002092495
kelvi <kelvi_ugo@hotmail.com>
2021-07-26 14:04:29 -0300

    test: finaliza os testes de delete

--- backend/src/modules/villages/services/DeleteVillageService.spec.ts
+++ backend/src/modules/villages/services/DeleteVillageService.spec.ts
@@ -1,18 +1,30 @@
 import { FakeVillageRepositories } from "../repositories/fakes/FakeVillageRepository";
+import { CreateVillageService } from "./CreateVillageService";
 import { DeleteVillageService } from "./DeleteVillageService";
 import { FindVillageServiceById } from "./FindByIDVillageService";
 
 describe('teste do DeleteVillageServices', () => {
     it('deleta vila por id', async () => {
         const fakeVillageRepository = new FakeVillageRepositories();
+        const createVillageService = new CreateVillageService(fakeVillageRepository);
         const deleteVillageServices = new DeleteVillageService(fakeVillageRepository);
-        const findVillageService = new FindVillageServiceById(fakeVillageRepository);
 
-        await deleteVillageServices.execute(1);
-        const villageFound = await findVillageService.execute(1);
-        if (villageFound) {
-        expect(villageFound).toBeInstanceOf(Error)
-        }
+        await createVillageService.execute(
+            {   
+                name: "ES",
+                country: "Brasil",
+              }
+        );
+        const VillageDeleted = await deleteVillageServices.execute(1);
+        expect(VillageDeleted).toBe(undefined)
 
     })
+    it('fala ao tentar deletar uma vila nao existenten', async () => {
+
+        const fakeVillageRepository = new FakeVillageRepositories();
+        const deleteVillageServices = new DeleteVillageService(fakeVillageRepository);
+        expect(await deleteVillageServices.execute(1)
+        ).toBeInstanceOf(Error)
+
+});
 });
--- backend/src/modules/villages/services/FindByIDVillageService.spec.ts
+++ backend/src/modules/villages/services/FindByIDVillageService.spec.ts
@@ -1,12 +1,24 @@
 import { FakeVillageRepositories } from "../repositories/fakes/FakeVillageRepository";
+import { CreateVillageService } from "./CreateVillageService";
 import { FindVillageServiceById } from "./FindByIDVillageService";
 
 describe('teste do FindVillageServiceByName', () => {
     it('buscar vila por id', async () => {
         const fakeVillageRepository = new FakeVillageRepositories();
         const findVillageService = new FindVillageServiceById(fakeVillageRepository)
+        const createVillageService = new CreateVillageService(
+            fakeVillageRepository
+          );
+      
+          await createVillageService.execute({
+            name: "ES",
+            country: "Brasil",
+          });
+
 
         const villageFound = await findVillageService.execute(1);
+
+
         if (villageFound) {
         expect(villageFound.name).toBe('folha')
         }
